a2429ba57f2a9968c6cf955353fde7abed73678b
FengJie <fllying8791@gmail.com>
2025-04-27 02:17:42 -0700

    finished all the app structure. finished 3D model animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,61 @@
 import MarsGlobe from './components/Mars.tsx';
+import { useEffect, useState } from 'react';
+import Lenis from '@studio-freight/lenis';
+import Hero from './components/Hero.tsx';
+import FirstTextSection from './components/FirstTextSection.tsx';
+import Weather from './components/Weather.tsx';
+import MarsGallery from './components/MarsGallery.tsx';
+import PictureOfTheDay from './components/PictureOfTheDay.tsx';
+import SecondTextSection from './components/SecondTextSection.tsx';
+import Footer from './components/Footer.tsx';
 
 function App() {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  // this useEffect make scroll down smooth
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <>
-      <MarsGlobe />
+      <h1 className='test-scrollTop'>{scrollTop}</h1>
+      <Hero />
+      <div className={`mars-canvas-container ${scrollTop > 800 ? 'hide' : ''}`}>
+        <MarsGlobe scrollTop={scrollTop} />
+        {/* <h1> Canvas here</h1> */}
+      </div>
+      <FirstTextSection />
+      <Weather scrollTop={scrollTop} />
+      <MarsGallery />
+      <SecondTextSection />
+      <PictureOfTheDay />
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <h1 className='section-title'>
+        Discover Mars: Where Exploration Meets Reality
+      </h1>
+      <h2 className='section-sub-title'>
+        Experience interactive 3D models, compare planetary environments, and
+        witness the stunning beauty of the Red Planet.
+      </h2>
+    </div>
+  );
+};
+
+export default Hero;
